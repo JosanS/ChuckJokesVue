f32033b1135c1a9887f71f9e0adabae3330f73c1
JosanS <josansalinas@hotmail.com>
2024-11-11 20:37:26 -0600

    actividad 12

---
+++ README.md
@@ -0,0 +1,6 @@
+## Actividad 12
+
+Se creo el proyecto e integro vue
+Se añadieron los chistes de chuck norris al proyecto mediante un "component" de vue
+Se vinculo este "component" a una vista y creo "stylesheet" basico
+
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Chistes de Chuck Norris</h1>
+        <ul>
+            <li v-for="joke in jokes" :key="joke.value">{{ joke.value }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jokes: [
+                { value: "Chuck Norris can skydive into outer space." },
+                { value: "The chief export of Chuck Norris is pain." },
+                { value: "Chuck Norris doesn't read books. He stares them down until he gets the information he wants." },
+                { value: "Time waits for no man. Unless that man is Chuck Norris." },
+                { value: "If you spell Chuck Norris in Scrabble, you win. Forever." },
+            ]
+        };
+    }
+};
+</script>
+
+<!--
+<style scoped>
+
+h1 {
+    color: #333;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    margin: 10px 0;
+}
+</style> -->
